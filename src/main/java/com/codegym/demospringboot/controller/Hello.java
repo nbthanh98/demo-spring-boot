2f0c3f2f19921937db388677b12ccb725fe7df4e
nbthanh98 <nguyenbathanh1280@gmail.com>
2019-05-10 14:51:05 +0700

    first commit

---
+++ src/main/java/com/codegym/demospringboot/controller/Hello.java
@@ -0,0 +1,17 @@
+package com.codegym.demospringboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/hello")
+public class Hello {
+    @GetMapping
+    public ModelAndView sayHello(){
+        ModelAndView modelAndView = new ModelAndView("hello");
+        modelAndView.addObject("name", "word");
+        return modelAndView;
+    }
+}
